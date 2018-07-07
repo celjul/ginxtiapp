a969aeca43406cabb0fcb336cef2d438d26d59a3
Ricardo Tapia <rtapia92@gmail.com>
2018-07-07 00:46:06 -0500

    Add general navigation icon buttons

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import {
   Image,
   StyleSheet,
+  TouchableOpacity,
   View
 } from 'react-native';
-import { PrimaryColor } from '../../styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { DarkPrimaryColor, PrimaryColor, NeonGreen } from '../../styles';
 
 type Props = {};
 export default class DashboardIndex extends Component<Props> {
@@ -15,8 +17,28 @@ export default class DashboardIndex extends Component<Props> {
           <Image source={require('../../assets/logo.png')}/>
         </View>
         <View style={styles.buttons}>
-          <View style={styles.iconButton}>
-          </View>
+          <TouchableOpacity style={styles.iconButton}>
+            <Icon name="calendar" size={70} color={NeonGreen}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconButton}>
+            <Icon name="user" size={70} color={NeonGreen} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttons}>
+          <TouchableOpacity style={styles.iconButton}>
+            <Icon name="list-alt" size={70} color={NeonGreen}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconButton}>
+            <Icon name="thumbs-up" size={70} color={NeonGreen} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.buttons}>
+          <TouchableOpacity style={styles.iconButton}>
+            <Icon name="users" size={70} color={NeonGreen}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.iconButton}>
+            <Icon name="volume-up" size={70} color={NeonGreen} />
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -35,10 +57,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   buttons: {
-    flex: 3,
+    flex: 1,
     backgroundColor: PrimaryColor,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
   },
   iconButton: {
-    width: '50%',
+    flex: 0.5,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: DarkPrimaryColor,
+    borderWidth: 2,
   },
 });
