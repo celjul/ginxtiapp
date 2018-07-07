69d8538bf1590bf0df0ba6de628eb1c48dfee6e2
Ricardo Tapia <rtapia92@gmail.com>
2018-07-06 19:07:24 -0500

    Add basic login view with assets

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from 'react';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import DropdownAlert from 'react-native-dropdownalert';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { NeonGreen, PrimaryColor, White } from './styles';
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  onSubmitLogin(){
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ScrollView style={styles.container} contentContainerStyle={styles.scrollViewContainer}>
+        <View style={styles.centeredImageContainer}>
+          <Image source={require('./assets/logo.png')}/>
+          <Image source={require('./assets/separator.png')}/>
+        </View>
+        <View style={styles.loginForm}>
+          <View style={styles.inputContainer}>
+            <Icon style={styles.icon} name="envelope" size={30} color="#FFF" />
+            <TextInput style={styles.input}
+              onChangeText={(text) => this.setState({ email: text })}
+              placeholder="usuario"
+              keyboardType="default"
+              value={this.state.email}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Icon style={styles.icon} name="lock" size={30} color="#FFF" />
+            <TextInput style={styles.input}
+              onChangeText={(text) => this.setState({ password: text })}
+              secureTextEntry={true}
+              placeholder="***********"
+              value={this.state.password}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.onSubmitLogin.bind(this)}>
+            <Text style={styles.buttonText}>
+              Enviar
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <DropdownAlert ref={ref => this.dropdown = ref} />
+      </ScrollView>
     );
   }
 }
@@ -32,18 +66,56 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    alignSelf: 'stretch',
+    backgroundColor: PrimaryColor,
+  },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  centeredImageContainer: {
+    flex: 2,
+    marginBottom: 20,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginForm: {
+    flex: 2,
+    width: '100%',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    marginVertical: 5,
+    paddingHorizontal: 30,
+  },
+  input: {
+    width: '80%',
+    backgroundColor: White,
+    borderRadius: 5,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    paddingTop: 7,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
     textAlign: 'center',
-    margin: 10,
   },
-  instructions: {
+  button: {
+    width: '50%',
+    marginTop: 20,
+    backgroundColor: NeonGreen,
+    paddingVertical: 9,
+    borderRadius: 9,
+  },
+  buttonText: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: White,
+    textTransform: 'uppercase',
   },
 });
---
+++ src/styles.js
@@ -0,0 +1,6 @@
+export const PrimaryColor = '#2C3F59';
+export const LightGray = '#C2C2C2';
+export const White = '#FFF';
+export const Black = '#000';
+export const NeonGreen = '#BCDA34';
+
