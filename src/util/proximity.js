38ae4d20aa284551cfcd00262bb3caae20151c6d
Ricardo Tapia <rtapia92@gmail.com>
2018-07-26 11:18:33 -0500

    add configuration values for beacones:

--- src/util/proximity.js
+++ src/util/proximity.js
@@ -1,26 +1,23 @@
 import * as RNEP from '@estimote/react-native-proximity';
 
 export function ProximityInitializer (){
-  let standZone = new RNEP.ProximityZone(1, 'type');
+  let standZone = new RNEP.ProximityZone(1, 'ginxti-n1d');
 
   standZone.onEnterAction = context => {
     console.log(context);
   };
+  standZone.onExitAction = context => { console.log(context); };
+  standZone.onChangeAction = context => { console.log(context); };
 
   RNEP.locationPermission.request().then(
     permission => {
       if(permission !== RNEP.locationPermission.DENIED) {
         const credentials = new RNEP.CloudCredentials(
-          'demobeaconbst-7ha',
-          '5f33a5a8672f2c674a36fc9aeea703c2'
+          'ginxti-n1d',
+          '7016110294e4a828896276177cc13c3a'
         );
 
-        const config = {
-          notification: {
-            title: 'Text',
-            text: 'Notification',
-          },
-        };
+        const config = {};
 
         RNEP.proximityObserver.initialize(credentials, config);
         RNEP.proximityObserver.startObservingZones([standZone]);
